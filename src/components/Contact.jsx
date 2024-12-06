2ff671eb7f04424dbad23eebfcd670ba1bcfaae0
cyrus_w <cyrusw89@gmail.com>
2024-12-07 02:41:37 +1300

    adjust: contact margins

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,8 @@ const ImageClipBox = ({ src, clipClass }) => (
 
 const Contact = () => {
   return (
-    <div id="contact" className="my-20 min-h-96 w-screen  px-10">
-      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+    <div id="contact" className="md:my-20 max-md:mt-20 min-h-96 w-screen md:px-10">
+      <div className="relative md:rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
             src="/img/contact-1.webp"
