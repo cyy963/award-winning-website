9502aa4bcf2d8cf6378003bd7472f98b6533a349
cyrus_w <cyrusw89@gmail.com>
2024-12-08 23:37:17 +1300

    adjust: hero, index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -100,7 +100,7 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg max-md:hidden">
             <VideoPreview>
               <div
                 onClick={handleMiniVdClick}
