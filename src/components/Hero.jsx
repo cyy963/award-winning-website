b4bbf5fd841c8cc327d5ce8a1695a3be00468890
cyrus_w <cyrusw89@gmail.com>
2024-12-12 19:39:21 +1300

    update: components; Navbar, Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
   };
 
   useEffect(() => {
-    if (loadedVideos >= totalVideos - 3) {
+    if (loadedVideos >= totalVideos - 1) {
       setLoading(false);
     }
   }, [loadedVideos]);
